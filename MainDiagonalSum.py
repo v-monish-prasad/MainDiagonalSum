1389e4f056b59930ad37d54c4a608c81490ffdea
Monish Prasad V <monishprasad2018@gmail.com>
2023-07-17 14:16:12 +0530

    Initial commit

---
+++ MainDiagonalSum.py
@@ -0,0 +1,28 @@
+def findMainDiagonalSum(matrix):
+    # if not elements:
+    #     return "Empty Array."
+
+    mainDiagonalSum = 0
+
+    for i in range(len(matrix)):
+        mainDiagonalSum += matrix[i][i]
+
+    return mainDiagonalSum
+
+
+if __name__ == "__main__":
+    inputList = list(map(int, input().split()))
+    noOfRows = inputList[0]
+    noOfColumns = inputList[1]
+    matrix = []
+
+    prevI = 0
+    for i in range(2, len(inputList) + noOfColumns, noOfColumns):
+        rows = []
+        for j in range(prevI, i):
+            rows.append(inputList[j])
+        prevI = i
+        if i != 2:
+            matrix.append(rows)
+
+    print(findMainDiagonalSum(matrix))
